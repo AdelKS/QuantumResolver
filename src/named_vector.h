b94486e0e54c27d6619991a5f899dca52765aec3
Adel KARA SLIMANE <adel.ks@zegrapher.com>
2022-06-11 16:54:03 +0200

    NamedVector: move implementation to header
    Cannot write template classes properly in .cpp files
    
    Signed-off-by: Adel KARA SLIMANE <adel.ks@zegrapher.com>

--- src/named_vector.h
+++ src/named_vector.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <unordered_map>
 #include <limits>
+#include <stdexcept>
 
 template <class Object>
 class NamedVector
@@ -12,18 +13,56 @@ class NamedVector
     // A vector that can either be accessed with vector[1] or vector[name], where name is a string
 
 public:
-    NamedVector();
+    typedef typename std::vector<Object>::iterator iterator;
+    typedef typename std::vector<Object>::const_iterator const_iterator;
 
-    std::size_t size();
-    bool empty();
-    Object& back();
-    auto begin();
-    auto end();
-    std::size_t id_of(const std::string_view &name) const;
-    bool contains(std::string_view name);
-    std::size_t push_back(Object object, std::string name);
-    Object& operator [](const std::string_view &name);
-    Object& operator [](std::size_t i);
+    NamedVector() {};
+
+    std::size_t size() const {return objects.size();};
+    bool empty() {return objects.back();};
+    Object& back() {return objects.back();};
+    iterator begin() {return objects.begin();};
+    iterator end() {return objects.end();};
+    const_iterator cbegin() const {return objects.cbegin();} ;
+    const_iterator cend() const {return objects.cend();};
+
+    std::size_t id_of(const std::string_view &name) const
+    {
+        auto it = name_to_index.find(name);
+        if( it != name_to_index.end())
+            return it->second;
+        return npos;
+    }
+
+    bool contains(std::string_view name)
+    {
+        return name_to_index.contains(name);
+    }
+
+    std::size_t push_back(Object object, std::string name)
+    {
+        std::size_t index = objects.size();
+        objects.push_back(std::move(object));
+        objects.back().set_id(index);
+
+        name_to_index[std::move(name)] = index;
+        return index;
+    }
+
+    Object& operator [](const std::string_view &name)
+    {
+        auto it = name_to_index.find(name);
+        if(it != name_to_index.end())
+        {
+            return objects[it->second];
+        }
+        else throw std::runtime_error("Accessing an unexisting object name: " + std::string(name));
+    }
+
+    Object& operator [](std::size_t i)
+    {
+        return objects[i];
+    }
 
     static constexpr std::size_t npos = std::numeric_limits<std::size_t>::max();
 
@@ -42,7 +81,5 @@ protected:
     std::vector<Object> objects;
     std::unordered_map<std::string, std::size_t, string_hash, std::equal_to<>> name_to_index;
 };
-
-#include "named_vector.cpp"
 
 #endif // NAMED_VECTOR_H
